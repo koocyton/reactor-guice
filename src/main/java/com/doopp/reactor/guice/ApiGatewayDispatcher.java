a2b49bfdc677445810ee680326c58a112755721e
yi.liu <liuyi@doopp.com>
2019-05-05 17:49:01 +0800

    继续处理网关模式

--- src/main/java/com/doopp/reactor/guice/ApiGatewayDispatcher.java
+++ src/main/java/com/doopp/reactor/guice/ApiGatewayDispatcher.java
@@ -2,4 +2,5 @@ package com.doopp.reactor.guice;
 
 public interface ApiGatewayDispatcher {
 
+    String getPrimaryAddress(String host);
 }
--- src/main/java/com/doopp/reactor/guice/publisher/ApiGatewayPublisher.java
+++ src/main/java/com/doopp/reactor/guice/publisher/ApiGatewayPublisher.java
@@ -1,11 +1,21 @@
 package com.doopp.reactor.guice.publisher;
 
 import com.doopp.reactor.guice.ApiGatewayDispatcher;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.cookie.Cookie;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.server.HttpServerRequest;
 import reactor.netty.http.server.HttpServerResponse;
 
+import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.Set;
+
 public class ApiGatewayPublisher {
+
+    private HttpClient httpClient = HttpClient.create();
 
     private ApiGatewayDispatcher apiGatewayDispatcher;
 
@@ -14,22 +24,41 @@ public class ApiGatewayPublisher {
     }
 
     public Mono<Object> sendResponse(HttpServerRequest req, HttpServerResponse resp) {
+
+        String uri = req.uri();
+        String host = this.apiGatewayDispatcher.getPrimaryAddress(uri);
+        Map<CharSequence, Set<Cookie>> cookie = req.cookies();
+        InetSocketAddress hostAddress = req.hostAddress();
+        InetSocketAddress remoteAddress = req.remoteAddress();
+        HttpHeaders requestHeaders = req.requestHeaders();
+
+        if (req.method()== HttpMethod.POST) {
+            return this.postResponse(host, uri, null);
+        }
+        else if (req.method()== HttpMethod.DELETE) {
+            return this.deleteResponse(host, uri);
+        }
+        else if (req.method()== HttpMethod.PUT) {
+            return this.putResponse(host, uri, null);
+        }
+        else {
+            return this.getResponse(host, uri);
+        }
+    }
+
+    private Mono<Object> getResponse(String host, String uri) {
+        return httpClient.cookie();
+    }
+
+    private Mono<Object> postResponse(String host, String uri, String postData) {
         return Mono.just("abc");
     }
 
-    public Mono<Object> getResponse(String host, String uri) {
+    private Mono<Object> deleteResponse(String host, String uri) {
         return Mono.just("abc");
     }
 
-    public Mono<Object> postResponse(String host, String uri, String postData) {
-        return Mono.just("abc");
-    }
-
-    public Mono<Object> deleteResponse(String host, String uri) {
-        return Mono.just("abc");
-    }
-
-    public Mono<Object> putResponse(String host, String uri, String postData) {
+    private Mono<Object> putResponse(String host, String uri, String postData) {
         return Mono.just("abc");
     }
 
