98f0db9f0b08704c46ab0d32025c30be9c948239
“yi.liu“ <yi.liu@kunlun-inc.com>
2020-02-07 11:37:36 +0800

    准备打包更新

--- src/test/java/com/doopp/reactor/guice/test/handle/WebsocketHandle.java
+++ src/test/java/com/doopp/reactor/guice/test/handle/WebsocketHandle.java
@@ -4,11 +4,9 @@ import com.doopp.reactor.guice.websocket.AbstractWebSocketServerHandle;
 import com.google.inject.Singleton;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.ws.rs.Path;
-import java.time.Duration;
 
 @Path("/kreactor/ws")
 @Singleton
@@ -16,15 +14,6 @@ public class WebsocketHandle extends AbstractWebSocketServerHandle {
 
     @Override
     public Mono<Void> onTextMessage(TextWebSocketFrame frame, Channel channel) {
-        for(int ii=0; ii<10; ii++) {
-            this.sendTextMessage(frame.text(), channel);
-        }
-        return Mono.empty();
-    }
-
-    @Override
-    public Mono<Void> onConnect(Channel channel) {
-        // Flux.interval(Duration.ofMillis(1000))
-        return super.onConnect(channel);
+        return this.sendTextMessage(frame.text(), channel);
     }
 }
